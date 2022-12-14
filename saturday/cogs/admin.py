48a82249c9cfbcc1a53401d708a5253859e96afa
Ken <83001955+audrickkena@users.noreply.github.com>
2022-12-14 14:08:17 +0800

    added debug statements

--- saturday/cogs/admin.py
+++ saturday/cogs/admin.py
@@ -35,9 +35,13 @@ class Admin(commands.Cog):
         message = ''
         for cogName, cog in cogs.items():
             message += f'{cogName} cog slash-commands:\n'
+            print(message)
             message += self.getAppCommands(cog)
+            print(message)
             message += f'{cogName} cog prefix-commands:\n'
+            print(message)
             message += self.getCommands(cog)
+            print(message)
             message += '\n'
         await ctx.send(content=message)
         
