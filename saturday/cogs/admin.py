4a5db7594f021153d39d4defd76fd473e1395f7d
Ken <83001955+audrickkena@users.noreply.github.com>
2022-12-14 18:54:41 +0800

    added reloadAll prefix command

--- saturday/cogs/admin.py
+++ saturday/cogs/admin.py
@@ -10,14 +10,23 @@ from discord import app_commands
 class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.initial_extensions = [
+            "cogs.utility",
+            "cogs.admin"
+        ]
 
-    def is_guild_owner():
+    def is_guild_owner_intr():
         def predicate(interaction: discord.Interaction):
             return interaction.guild is not None and interaction.guild.owner_id == interaction.user.id
         return app_commands.check(predicate)
 
+    def is_guild_owner_ctx():
+        def predicate(ctx):
+            return ctx.guild is not None and ctx.guild.owner_id == ctx.author.id
+        return app_commands.check(predicate)
+
     @app_commands.command(name="reload", description="For reloading cogs")
-    @is_guild_owner()
+    @is_guild_owner_intr()
     async def reload(self, interaction: discord.Interaction, module: str):
         try:
             await self.bot.reload_extension(module)
@@ -29,11 +38,24 @@ class Admin(commands.Cog):
     async def reload_error(self, interaction: discord.Interaction, error):
         await interaction.response.send_message(f'You do not have the necessary permissions to access /{interaction.command.name}. If this is not the intended effect, please contact the server admin.', ephemeral=True)
 
+    @commands.command(name="reloadAll", description="For reloading all cogs")
+    @is_guild_owner_ctx()
+    async def reloadAll(self, ctx):
+        try:
+            for ext in self.initial_extensions:
+                await self.bot.reload_extension(ext)
+            print("All cogs reloaded successfully")
+        except commands.ExtensionError as e:
+            print(e)
+    @reloadAll.error
+    async def reloadAll_error(self, ctx, error):
+        print(f'{ctx.author.display_name} does not have the necessary permissions to access !{ctx.command.name}.')
+
     @commands.command(name="clear", description="For clearing app commands", usage="!clear")
     async def clear(self, ctx):
         fmt = await ctx.bot.tree.clear_commands(guild=ctx.guild)
         await ctx.send(f'Cleared {len(fmt)} commands.')
-        
+
     @commands.command(name="sync", description="For syncing app commands", usage="!sync")
     async def sync(self, ctx):
         
