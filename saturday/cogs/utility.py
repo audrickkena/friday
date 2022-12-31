9775ce4528ae9f42f4e1a2561886bbe6802bca7f
audrickkena <audrickkena@gmail.com>
2022-12-31 16:17:49 +0800

    removed footee

--- saturday/cogs/utility.py
+++ saturday/cogs/utility.py
@@ -96,7 +96,6 @@ class PollModal(Modal):
             title=f'Poll: {self.pollName}',
             color=discord.Colour.blue()
         )
-        message.set_footer(text=f'Poll made by: {interaction.user.display_name}')
         for i in range(len(self.pollOptions)):
             message.add_field(name=f'Option {i+1}:', value=f'{self.pollOptions[i]}', inline=False)
         await interaction.response.send_message(embeds=message, ephemeral=True)
