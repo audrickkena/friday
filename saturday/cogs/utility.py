613fa52e1e62c3a8e9161e4ff43b899a0b60da0d
audrickkena <audrickkena@gmail.com>
2022-12-31 16:56:25 +0800

    fixing embed title

--- saturday/cogs/utility.py
+++ saturday/cogs/utility.py
@@ -82,8 +82,10 @@ class PollModal(Modal):
             title='Make A Poll'
         )
         self.pollOptions = []
-        self.pollName = self.add_item(TextInput(label='Poll name:', max_length=50, required=True))
-        self.pollDesc = self.add_item(TextInput(label='Poll description:', max_length=50, required=False))
+        self.pollName = TextInput(label='Poll name:', max_length=50, required=True)
+        self.pollDesc = TextInput(label='Poll description:', max_length=50, required=False)
+        self.add_item(self.pollName)
+        self.add_item(self.pollDesc)
         for i in range(numOfOptions):
             temp = TextInput(label=f'Option {i + 1}:', max_length=50, required=True)
             self.pollOptions.append(temp)
