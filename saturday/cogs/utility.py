41f743bc429dc3be085e10269a949c715bcbe76a
audrickkena <audrickkena@gmail.com>
2023-01-02 16:37:21 +0800

    changed poll implementation

--- saturday/cogs/utility.py
+++ saturday/cogs/utility.py
@@ -84,14 +84,19 @@ class PollModal(Modal):
         self.pollOptions = []
         self.pollName = TextInput(label='Poll name:', max_length=50, required=True)
         self.pollDesc = TextInput(label='Poll description:', max_length=50, required=False)
+        self.pollOptions = TextInput(
+            label='Poll options: (enter options separated by commas, captilization will be done automatically)', 
+            max_length=2000, 
+            style=discord.TextStyle.paragraph, 
+            placeholder='Example: (option1,option2,option3)\nOutput:\nOption1\nOption2\nOption3')
         self.add_item(self.pollName)
         self.add_item(self.pollDesc)
-        for i in range(numOfOptions):
-            temp = TextInput(label=f'Option {i + 1}:', required=True, row=(i%3)+2)
-            self.pollOptions.append(temp)
-            self.add_item(temp)
+        self.add_item(self.pollOptions)
 
     async def on_submit(self, interaction: discord.Interaction):
+        self.pollOptions = self.pollOptions.split(",")
+        for option in self.pollOptions: #remove trailing and leading whitespace per option
+            option = option.strip()
         message = discord.Embed(
             title=f'Poll: {self.pollName}',
             color=discord.Colour.blue())
