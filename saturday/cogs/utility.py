9bb76800f36835a28ff54416e453c3469b1e2076
Ken <83001955+audrickkena@users.noreply.github.com>
2023-01-09 04:19:46 +0800

    fixing issue with add_reaction in poll

--- saturday/cogs/utility.py
+++ saturday/cogs/utility.py
@@ -97,7 +97,7 @@ class PollModal(Modal):
         self.emojis = []
 
     def initEmojis(self):
-        infile = open('cogs/resources/emojis copy.txt', 'r')
+        infile = open('cogs/resources/emojis.txt', 'r')
         for line in infile:
             if line != '::':
                 self.emojis.append(line[:-1])
@@ -116,13 +116,12 @@ class PollModal(Modal):
             temp = random.choice(self.emojis)
             while(temp in self.emojiList):
                 temp = random.choice(self.emojis)
-            self.emojiList.append(temp)
-            message.add_field(name=f'\u200b', value=f'Option {emoji.emojize(self.emojiList[i])}: {self.pollOptions[i]}', inline=False)
+            self.emojiList.append(emoji.emojize(temp))
+            message.add_field(name=f'\u200b', value=f'Option {self.emojiList[i]}: {self.pollOptions[i]}', inline=False)
         await interaction.response.send_message(embed=message)
         sent = await interaction.original_response()
         for i in range(len(self.emojiList)):
-            print(self.emojiList[i])
-            await sent.add_reaction(self.emojiList[i].decode('string_escape'))
+            await sent.add_reaction(self.emojiList[i])
 
 
 
