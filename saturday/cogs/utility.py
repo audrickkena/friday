65390656982efe5054a1c161baa525afa18712b9
Ken <83001955+audrickkena@users.noreply.github.com>
2023-01-09 04:06:40 +0800

    fixing issue with add_reaction in poll

--- saturday/cogs/utility.py
+++ saturday/cogs/utility.py
@@ -121,8 +121,7 @@ class PollModal(Modal):
         await interaction.response.send_message(embed=message)
         sent = await interaction.original_response()
         for i in range(len(self.emojiList)):
-            print(emoji.emojize(':smile:'))
-            await sent.add_reaction(emoji.emojize(':smile:'))
+            await sent.add_reaction("\U0001f600")
 
 
 
