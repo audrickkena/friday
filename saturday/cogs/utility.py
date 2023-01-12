07afe2ce1a5c267452d7749f5c9974fb31fe8b28
Ken <83001955+audrickkena@users.noreply.github.com>
2023-01-12 19:21:01 +0800

    fixed add_reaction not working

--- saturday/cogs/utility.py
+++ saturday/cogs/utility.py
@@ -120,9 +120,7 @@ class PollModal(Modal):
         sent = await interaction.original_response()
         for i in range(len(self.emojiList)):
             temp = self.emojiList[i]
-        await sent.add_reaction('😀')
-        await sent.add_reaction('😄')
-        await sent.add_reaction('😁')
+        await sent.add_reaction(😀)
 
 
 
