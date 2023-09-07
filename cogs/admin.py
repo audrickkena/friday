6347d8d805fde70a62ccef8bbbce7188dca0a6fe
Ken <83001955+audrickkena@users.noreply.github.com>
2023-09-08 04:00:23 +0800

    debugging removeBackup

--- cogs/admin.py
+++ cogs/admin.py
@@ -119,7 +119,7 @@ class Admin(commands.Cog):
             # for uID in rolesDict:
             #     if rolesDict[uID] == user:
             #         rolesDict.pop(uID)
-        namesDict.pop(uID)
+        namesDict.pop(delete)
         print(namesDict)
         namesFile.close()
         namesFile = open('backups/memberNamesBackup.json', 'w')
