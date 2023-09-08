98a92970a2dec92731aeaaa5457e2e24c7cfd79a
Ken <83001955+audrickkena@users.noreply.github.com>
2023-09-08 23:20:56 +0800

    debugging on submit

--- cogs/utility.py
+++ cogs/utility.py
@@ -94,6 +94,7 @@ class addDictModal(discord.ui.Modal, title='Add a word/phrase'):
             date = currDateTime.strftime('%x')
             time = currDateTime.strftime('%X')
             if len(f.readlines()) > 0:
+                f.seek(0)
                 prevDict = json.loads(f.read())
                 print(f'prevDict = {prevDict}')
                 prevDict[self.word.value.lower()] = f'{self.meaning.value},{self.usage.value},{date},{time}'
