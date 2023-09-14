61d7a8299f2a30962d1eb7f8cae88d752f4ee2d8
Ken <83001955+audrickkena@users.noreply.github.com>
2023-09-15 04:06:30 +0800

    updated selamat pagi to remove role of person who sent the interaction

--- cogs/misc.py
+++ cogs/misc.py
@@ -57,11 +57,11 @@ class Misc(commands.Cog):
                     rudeRole = discord.utils.get(interaction.guild.roles, name=f'rude to {member.display_name}')
                     if rudeRole != None:
                         if sender.get_role(rudeRole.id) != None:
-                            await member.remove_roles(role)
-                            await interaction.response.send_message(f'{user_mention} you have been greeted back by <@{sender}>')
+                            await sender.remove_roles(role)
+                            await interaction.response.send_message(f'{user_mention} you have been greeted back by <@{sender.display_name}>')
                             return
                 await member.add_roles(role)
-                await interaction.response.send_message(f'{user_mention} you have been greeted by <@{sender}>')
+                await interaction.response.send_message(f'{user_mention} you have been greeted by <@{sender.display_name}>')
 
     def checkTime(self):
         currDateTime = datetime.datetime.now() + datetime.timedelta(hours=8)
