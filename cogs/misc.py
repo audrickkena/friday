2882328e184be045cfbc98b15597447b536f49a2
Ken <83001955+audrickkena@users.noreply.github.com>
2023-09-15 04:44:06 +0800

    used hi command to check for status of user

--- cogs/misc.py
+++ cogs/misc.py
@@ -16,7 +16,7 @@ class Misc(commands.Cog):
 
     @app_commands.command(name='hi', description="For lonely people")
     async def hi(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'hello {discord.utils.get(interaction.client.get_all_members(), id=interaction.user.id).status}', ephemeral=True)
+        await interaction.response.send_message(f'hello PC:{interaction.user.desktop_status}\nhello mobile: {interaction.user.mobile_status}', ephemeral=True)
 
     @app_commands.command(name='ping', description="For really bored people")
     async def ping(self, interaction: discord.Interaction):
