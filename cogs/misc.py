5ce6bc545e68142928241cbf749aad8a31b02085
Ken <83001955+audrickkena@users.noreply.github.com>
2023-10-05 00:13:47 +0800

    testing error handling for missing roles

--- cogs/misc.py
+++ cogs/misc.py
@@ -144,7 +144,7 @@ class Misc(commands.Cog):
                 await self.greet(interaction, sender, member, user_mention, greeting)
 
     @selamatGrp.command(name='malam', description="For greeting a fellow member in the evening")
-    @app_commands.checks.has_any_role('Lvl 100 Mafia Warlord', 'Lvl 10 Boss')
+    @app_commands.checks.has_any_role('Lvl 10 Boss')
     async def malam(self, interaction: discord.Interaction, user_mention: str):
         if user_mention[1] != '@' or user_mention[2] == '&':
             await interaction.response.send_message(f'{user_mention} is not a mention of a user in the server! Type @{{username}} to ensure that user is mention properly!', ephemeral=True)
@@ -239,6 +239,13 @@ class Misc(commands.Cog):
     ##################################
     async def cog_app_command_error(self, interaction: discord.Interaction, error):
         print(str(error))
+        if isinstance(error, app_commands.MissingAnyRole):
+            missing = []
+            user = interaction.guild.get_member(interaction.user.id)
+            for role in error.missing_roles:
+                print(role)
+                if discord.utils.get(user.roles, name=role) == None:
+                    missing.append(role)
         await interaction.response.send_message(f'/{interaction.command.qualified_name} is broken! Please contact the admin about this issue!', ephemeral=True)
 
 
