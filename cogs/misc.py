a513cc7488073ca67496f6f257fe7d58b01cbea9
Ken <83001955+audrickkena@users.noreply.github.com>
2023-10-04 20:23:49 +0800

    trying out checks for app commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -113,7 +113,7 @@ class Misc(commands.Cog):
     #####################################################################
 
     @selamatGrp.command(name='pagi', description="For greeting a fellow member in the morning")
-    @hasRoles(['lvl 100 mafia warlord'])
+    @hasRoles(['Lvl 100 Mafia Warlord'])
     async def pagi(self, interaction: discord.Interaction, user_mention: str):
         if user_mention[1] != '@' or user_mention[2] == '&':
             await interaction.response.send_message(f'{user_mention} is not a mention of a user in the server! Type @{{username}} to ensure that user is mention properly!', ephemeral=True)
@@ -129,7 +129,7 @@ class Misc(commands.Cog):
                 await self.greet(interaction, sender, member, user_mention, greeting)
 
     @selamatGrp.command(name='petang', description="For greeting a fellow member in the afternoon")
-    @hasRoles(['lvl 100 mafia warlord'])
+    @hasRoles(['Lvl 100 Mafia Warlord'])
     async def petang(self, interaction: discord.Interaction, user_mention: str):
         if user_mention[1] != '@' or user_mention[2] == '&':
             await interaction.response.send_message(f'{user_mention} is not a mention of a user in the server! Type @{{username}} to ensure that user is mentioned properly!', ephemeral=True)
@@ -145,7 +145,7 @@ class Misc(commands.Cog):
                 await self.greet(interaction, sender, member, user_mention, greeting)
 
     @selamatGrp.command(name='malam', description="For greeting a fellow member in the evening")
-    @hasRoles(['lvl 100 mafia warlord'])
+    @hasRoles(['Lvl 100 Mafia Warlord'])
     async def malam(self, interaction: discord.Interaction, user_mention: str):
         if user_mention[1] != '@' or user_mention[2] == '&':
             await interaction.response.send_message(f'{user_mention} is not a mention of a user in the server! Type @{{username}} to ensure that user is mention properly!', ephemeral=True)
