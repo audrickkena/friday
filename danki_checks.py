b3176e8fd86e6d645cae51cff4d01c698fa58186
Ken <83001955+audrickkena@users.noreply.github.com>
2023-10-09 22:49:49 +0800

    updated checkRequired to check all required options in all modules of setup

--- danki_checks.py
+++ danki_checks.py
@@ -2,22 +2,31 @@ import json
 import danki_exceptions
 import discord
 
+'''
+for SETUP.json,
+str are used to hold channel names
+list are used to hold roles
+
+'''
+
 def check_has_role():
     pass
 
-def checkRequired(moduleName):
+def checkRequired():
     with open('SETUP.json', 'r') as f:
-        moduleSetup = json.loads(f.read())[moduleName]
-        for e in moduleSetup['required'].keys():
-            if type(moduleSetup['required'][e]) == str:
-                if moduleSetup['required'][e] == '---NONE---':
-                    raise danki_exceptions.MissingValueInSetup(e)
-            elif type(moduleSetup['required'][e]) == list:
-                if len(moduleSetup['required'][e]) == 1:
-                    if moduleSetup['required'][e][0] == '---NONE---':
+        setup = json.loads(f.read())
+        for module in setup.keys():
+            moduleSetup = setup[module]
+            for e in moduleSetup['required'].keys():
+                if type(moduleSetup['required'][e]) == str:
+                    if moduleSetup['required'][e] == '---NONE---':
                         raise danki_exceptions.MissingValueInSetup(e)
-                if '---NONE---' in moduleSetup['required'][e]:
-                    raise danki_exceptions.DefaultValueNotRemoved(e)
+                elif type(moduleSetup['required'][e]) == list:
+                    if len(moduleSetup['required'][e]) == 1:
+                        if moduleSetup['required'][e][0] == '---NONE---':
+                            raise danki_exceptions.MissingValueInSetup(e)
+                    if '---NONE---' in moduleSetup['required'][e]:
+                        raise danki_exceptions.DefaultValueNotRemoved(e)
         return moduleSetup
     
 def checkServerHasRequiredRoles(guild):
@@ -27,7 +36,7 @@ def checkServerHasRequiredRoles(guild):
             req = setup[module]['required']
             for option in req.keys():
                 if type(req[option]) == str:
-                    if discord.utils.get(guild.roles, name=req[option]) == None:
+                    if discord.utils.get(guild.text_channels, name=req[option]) == None:
                         raise danki_exceptions.RoleDoesNotExist(option, req[option])
                 elif type(req[option]) == list:
                     for role in req[option]:
