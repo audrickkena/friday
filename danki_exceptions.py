7661c756ba45bb8bb12741b742fc039f7c0047c5
Ken <83001955+audrickkena@users.noreply.github.com>
2023-10-09 22:51:07 +0800

    added exception if channel does not exist

--- danki_exceptions.py
+++ danki_exceptions.py
@@ -48,3 +48,22 @@ class RoleDoesNotExist(Exception):
     
     def getRole(self):
         return self.role_missing
+    
+class ChannelDoesNotExist(Exception):
+    ''' Exception raised when a channel specified in the SETUP.json does not exist in the server the bot is in
+    
+    Attributes:
+        var_key = name of the variable that contains the channel that is missing in the server
+        chan_missing = name of the channel missing from the server
+    '''
+    def __init__(self, var_key, chan_missing):
+        self.var_key = var_key
+        self.chan_missing = chan_missing
+        self.message = f'{tm_color.colors.fg.red}[ERROR]: {{{self.var_key}}} has a channel ({self.chan_missing}) that is not present in this server! Please check the channel and ensure it matches a channel in the server exactly (case sensitive){tm_color.colors.reset}'
+        super().__init__(self.message)
+    
+    def getKey(self):
+        return self.var_key
+    
+    def getRole(self):
+        return self.chan_missing
