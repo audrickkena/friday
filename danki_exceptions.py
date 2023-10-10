fe27e4911603eaa63ac655a766ea957fb249aaad
Ken <83001955+audrickkena@users.noreply.github.com>
2023-10-10 21:01:31 +0800

    changed var name of var_key to option_key for clarity and added setting_key for DEFAULTval exception

--- danki_exceptions.py
+++ danki_exceptions.py
@@ -6,56 +6,61 @@ class MissingValueInSetup(Exception):
     
     Attributes:
         module_key = name of the setup module the missing variable is from
-        var_key = name of the variable missing a value in SETUP.json
+        option_key = name of the option missing a value in SETUP.json
     '''
 
-    def __init__(self, module_key, var_key):
-        self.var_key = var_key
+    def __init__(self, module_key, option_key):
+        self.option_key = option_key
         self.module_key = module_key
-        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.ERROR} {{{self.var_key}}} from {{{self.module_key}}} has not been initialised although it is required! Please initialise this value before starting Danki!{tm_color.colors.reset}'
+        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.ERROR} {{{self.option_key}}} from {{{self.module_key}}} has not been initialised although it is required! Please initialise this value before starting Danki!{tm_color.colors.reset}'
         super().__init__(self.message)
 
     def getModule(self):
         return self.module_key
 
     def getKey(self):
-        return self.var_key
+        return self.option_key
 
 class DefaultValueNotRemoved(Exception):
     ''' Exception raised when the default value in setup file for an option is still present when user has updated the option
     
     Attributes:
         module_key = name of the setup module the missing variable is from
-        var_key = name of the variable that still has the default value in SETUP.json
+        setting_key = whether it is in the required setting or optional setting
+        option_key = name of the option that still has the default value in SETUP.json
     '''
 
-    def __init__(self, module_key, var_key):
-        self.var_key = var_key
+    def __init__(self, module_key, setting_key, option_key):
+        self.option_key = option_key
         self.module_key = module_key
-        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.WARNING} {{{self.var_key}}} from {{{self.module_key}}} still contains the default value! This value will now be removed for you!'
+        self.setting_key = setting_key
+        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.WARNING} {{{self.option_key}}} from {{{self.module_key}}} still contains the default value! This value will now be removed for you!'
         super().__init__(self.message)
 
     def getModule(self):
         return self.module_key
+    
+    def getSetting(self):
+        return self.setting_key
 
     def getKey(self):
-        return self.var_key
+        return self.option_key
     
 class RoleDoesNotExist(Exception):
     ''' Exception raised when a role specified in the SETUP.json does not exist in the server the bot is in
     
     Attributes:
-        var_key = name of the variable that contains the role that is missing in the server
+        option_key = name of the option that contains the role that is missing in the server
         role_missing = name of the role missing from the server
     '''
-    def __init__(self, var_key, role_missing):
-        self.var_key = var_key
+    def __init__(self, option_key, role_missing):
+        self.option_key = option_key
         self.role_missing = role_missing
-        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.ERROR} {{{self.var_key}}} has a role ({self.role_missing}) that is not present in this server! Please check the role and ensure it matches a role in the server exactly (case sensitive)'
+        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.ERROR} {{{self.option_key}}} has a role ({self.role_missing}) that is not present in this server! Please check the role and ensure it matches a role in the server exactly (case sensitive)'
         super().__init__(self.message)
     
     def getKey(self):
-        return self.var_key
+        return self.option_key
     
     def getRole(self):
         return self.role_missing
@@ -64,19 +69,19 @@ class ChannelDoesNotExist(Exception):
     ''' Exception raised when a channel specified in the SETUP.json does not exist in the server the bot is in
     
     Attributes:
-        var_key = name of the variable that contains the channel that is missing in the server
+        option_key = name of the option that contains the channel that is missing in the server
         chan_missing = name of the channel missing from the server
     '''
-    def __init__(self, var_key, chan_missing):
-        self.var_key = var_key
+    def __init__(self, option_key, chan_missing):
+        self.option_key = option_key
         self.chan_missing = chan_missing
-        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.ERROR} {{{self.var_key}}} has a channel ({self.chan_missing}) that is not present in this server! Please check the channel and ensure it matches a channel in the server exactly (case sensitive)'
+        self.message = f'{danki_enums.Console.getPrefix()} {danki_enums.Console.ERROR} {{{self.option_key}}} has a channel ({self.chan_missing}) that is not present in this server! Please check the channel and ensure it matches a channel in the server exactly (case sensitive)'
         super().__init__(self.message)
     
     def getKey(self):
-        return self.var_key
+        return self.option_key
     
-    def getRole(self):
+    def getChannel(self):
         return self.chan_missing
     
 class MemberMissingRole(Exception):
