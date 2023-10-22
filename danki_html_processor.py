0b9849a1abe8c499c985dd5ea5863784ba698a8e
Ken <83001955+audrickkena@users.noreply.github.com>
2023-10-22 22:15:18 +0800

    updated html process to by async instead of sync by using aiohttp instead of requests library

--- danki_html_processor.py
+++ danki_html_processor.py
@@ -1,21 +1,9 @@
-import requests
+import aiohttp
 from bs4 import BeautifulSoup
 
-# function for getting the title of a video directly from the webpage because pytube is not doing it
-async def getYoutubeTitle(link: str) -> str:
-    try:
-        page = requests.get(link)
-
-        # if the request was succesful
-        if page.status_code >= 200 and page.status_code < 300:
-            
-            # get a beautiful soup obj from the page using python's build in html parser
-            soup = BeautifulSoup(page.text, 'html.parser')
-
-            # title of video is in a meta tag, specifically <meta name="title"> so to get the title, 
-            # need to access this tag's content then return the result
-            title = soup.find(name="meta", attrs={"property": "og:title"}).get("content")
-            return title
-
-    except Exception as err:
-        raise err
+async def getYoutubeTitle(session: aiohttp.ClientSession, url: str) -> str:
+    async with session.get(url) as response:
+        page = await response.text()
+        soup = BeautifulSoup(page, 'html.parser')
+        title = soup.find(name="meta", attrs={"property": "og:title"}).get("content")
+        return title
